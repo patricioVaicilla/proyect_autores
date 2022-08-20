d5d931f0bff2cd5193303b9e4a12d76d3ad662d3
Patricio_Vaicilla <patoloco1200@gmail.com>
2022-08-20 09:18:09 -0500

    tercer commit

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const cors = require('cors');
 const app = express();
 const port = 8000;
 
-require('./config/mongoose.config');
+require('./server/config/mongoose.config');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const userAuthor=require('./server/routes/author.routes');
+userAuthor(app);
 
 app.listen(port, () => console.log(`Servidor escuchando en el puerto ${port}...`))
--- server/routes/author.routes.js
+++ server/routes/author.routes.js
@@ -3,7 +3,7 @@ const AuthorController = require('../controllers/author.controller');
 module.exports = function (app) {
     app.post('/api/author/new', AuthorController.createAuthor);
     app.get('/api/authors', AuthorController.getAllAuthors);
-    app.get('/api/author/:id', AuthorController.getAuhor);
+    app.get('/api/author/:id', AuthorController.getAuthor);
     app.put('/api/author/:id/', AuthorController.updateAuthor);
     app.delete('/api/author/:id', AuthorController.deleteAuthor);
 
